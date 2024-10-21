4c300939ec1cbc958ee1758b130e17139b90f6dd
Mohamed-Elmougy11 <mohamed.elmougy11@gmail.com>
2024-10-22 01:04:43 +0300

    chore: update to v6.4 Data APIs in react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Navigate,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 import AddPostForm from "./features/posts/AddPostForm";
 import PostsList from "./features/posts/PostsList";
 import Layout from "./components/Layout";
-import { Routes, Route, Navigate } from "react-router-dom";
 import SinglePostPage from "./features/posts/SinglePostspage";
 import EditPostForm from "./features/posts/EditPostForm";
 import UsersList from "./features/users/UsersList";
 import UserPage from "./features/users/UserPage";
 
-function App() {
-  return (
-    <Routes>
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
       <Route path="/" element={<Layout />}>
         <Route index element={<PostsList />} />
 
@@ -24,13 +30,14 @@ function App() {
           <Route path=":userId" element={<UserPage />} />
         </Route>
 
-
         {/* Catch all - replace with 404 component if you want */}
         <Route path="*" element={<Navigate to="/" replace />} />
-
       </Route>
-    </Routes>
-  );
+    </Route>
+  )
+);
+function App() {
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -4,17 +4,11 @@ import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./app/store.js";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router>
-        <Routes>
-          <Route path="/*" element={<App />} />
-        </Routes>
-      </Router>
+      <App />
     </Provider>
   </React.StrictMode>
 );
